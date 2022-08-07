232196fa93ab98a54d76ca481b46a941104b6522
serenade010 <lukechu010@gmail.com>
2022-08-07 16:18:30 +0800

    fix main page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import Home from './Home';
+// import Home from './Home';
 import Account from './Account';
 import Model from './Pages/Model';
 import News from './Pages/News';
@@ -10,8 +10,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Account />} />
+        <Route path="/" element={<Account />} />
         <Route path="/model" element={<Model />} />
         <Route path="/news" element={<News />} />
         <Route path="/price" element={<Price />} />
